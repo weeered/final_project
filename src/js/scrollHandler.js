b4d29801fd25a2c07ccd5cd95d44b55f73f42c2e
weeered <qturtle22@gmail.com>
2024-01-25 19:25:05 +0300

    update1

--- src/js/scrollHandler.js
+++ src/js/scrollHandler.js
@@ -7,23 +7,24 @@ const triggerElement = document.querySelector(".section__top_fixed");
 function changeHeaderColor() {
 	if (
 		window.scrollY >
-		triggerElement.offsetTop + triggerElement.offsetHeight - header.offsetHeight - 30
+		triggerElement.offsetTop +
+			triggerElement.offsetHeight -
+			header.offsetHeight -
+			150
 	) {
 		header.style.backgroundColor = "#342E8C";
-
 		header.style.position = "sticky";
 		header.style.left = "0";
-		header.style.top = "0";
+		header.style.transform = "translate(0, 0)";
 
 		button.style.transform = "translate(0, 0)";
-		console.log("up");
 	} else {
-		button.style.transform = "translate(0, 150px)";
-
 		header.style.backgroundColor = "transparent";
-		header.style.position = "relative";
+		header.style.position = "absolute";
+		header.style.left = "50%";
+		header.style.transform = "translate(-50%, 0)";
 
-		console.log("down");
+		button.style.transform = "translate(0, 150px)";
 	}
 }
 
